042a311b83b90ccd18b49723b1da7b0991a4da16
aos83137 <qazwsx1455@gmail.com>
2024-10-20 02:24:57 +0900

    wip

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -169,3 +169,10 @@ lightbox.addEventListener('click', (e) => {
 });
 
 // page4
+
+
+// page7 참석유무
+const { Client } = require('@notionhq/client');
+
+// const notion = new Client({ auth: process.env.NOTION_API_KEY });
+console.log(process.env.NOTION_API_KEY);
